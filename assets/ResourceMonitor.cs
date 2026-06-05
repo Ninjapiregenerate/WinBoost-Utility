ad0a825ac1172a09bb59773642f21f1afef45044
Ninjapiregenerate <dennisgroome71@gmail.com>
2026-06-05 22:41:15 +0300

    Create ResourceMonitor.cs

---
+++ assets/ResourceMonitor.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+
+namespace WinBoostUtility
+{
+    public class ResourceMonitor
+    {
+        public float GetCpuUsage()
+        {
+            return 0f;
+        }
+
+        public float GetMemoryUsage()
+        {
+            return 0f;
+        }
+    }
+}
